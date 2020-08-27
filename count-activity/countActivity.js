8cf632f9338d88f1b7e3aa6b874a2cfa2de05370
DenisEps <denis.epshteyn@gmail.com>
2020-08-27 11:50:26 +0300

    optimized likes algo

--- count-activity/countActivity.js
+++ count-activity/countActivity.js
@@ -1,7 +1,9 @@
 const fetch = require('node-fetch');
 
-const token = '6d86abe678ce7524fd761b7bd8566a8d86b88f8b8174042d7b9660526102f1dee48b771ffe2b7ca98529b';
+const token = '0d50473f2681355889a936e3c5a29d5b38c2e98f4ae8afd897a88fa619e0293537ee4940eb2187940a336';
 const groupId = '-117680198';
+// const groupId = '-198233594'
+// const postIds = ['1']
 const postIds = ['20844', '20826', '20818'];
 
 const userLikes = {};
@@ -9,15 +11,15 @@ const userComments = {};
 
 // LIKES
 async function countLikesForPost(postId) {
-  const response = await fetch(`https://api.vk.com/method/likes.getList?&type=post&access_token=${token}&owner_id=${groupId}&item_id=${postId}&extended=1&offset=0&v=5.122&count=1000`);
+  const response = await fetch(`https://api.vk.com/method/likes.getList?&type=post&access_token=${token}&owner_id=${groupId}&item_id=${postId}&offset=0&v=5.122&count=1000`);
   const final = await response.json();
   const likesArray = final.response.items;
 
-  for (const likeObj of likesArray) {
-    if (likeObj.id in userLikes) {
-      userLikes[likeObj.id] += 1;
-    } else if (!(likeObj.id in userLikes)) {
-      userLikes[likeObj.id] = 1;
+  for (const id of likesArray) {
+    if (id in userLikes) {
+      userLikes[id] += 1;
+    } else if (!(id in userLikes)) {
+      userLikes[id] = 1;
     }
   }
 }
@@ -29,18 +31,17 @@ async function countLikesAllPosts(postsArray) {
 }
 
 // COMMENTS
-//TODO if groups comment? final.response.groups
 // TODO comments for comments?
 async function countCommentsForPost(postId) {
-  const response = await fetch(`https://api.vk.com/method/wall.getComments?&owner_id=${groupId}&post_id=${postId}&need_likes=1&extended=1&access_token=${token}&v=5.122&count=100`)
+  const response = await fetch(`https://api.vk.com/method/wall.getComments?&owner_id=${groupId}&post_id=${postId}&need_likes=1&access_token=${token}&v=5.122&count=100`)
   const final = await response.json();
-  const commentsArray = final.response.profiles;
+  const commentsArray = final.response.items;
 
   for (const commentObj of commentsArray) {
-    if (commentObj.id in userComments) {
-      userComments[commentObj.id] += 1;
-    } else if (!(commentObj.id in userComments)) {
-      userComments[commentObj.id] = 1;
+    if (commentObj.from_id in userComments) {
+      userComments[commentObj.from_id] += 1;
+    } else if (!(commentObj.from_id in userComments)) {
+      userComments[commentObj.from_id] = 1;
     }
   }
 }
@@ -51,5 +52,5 @@ async function countCommentsAllPosts(postsArray) {
   }
 }
 
-countLikesAllPosts(postIds).then(_ => console.log(userLikes))
-countCommentsAllPosts(postIds).then(_ => console.log(userComments))
+countLikesAllPosts(postIds).then(_ => console.log(userLikes));
+countCommentsAllPosts(postIds).then(_ => console.log(userComments));
