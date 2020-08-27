aed52e987edb797350d4da4b5c62c0b9991d9fdc
DenisEps <denis.epshteyn@gmail.com>
2020-08-27 19:41:11 +0300

    hbs

---
+++ count-activity/mergeActivity.js
@@ -0,0 +1,16 @@
+const mergedResults = (arr) => {
+  const result = {};
+
+  for (const array of arr) {
+    for (const [id, count] of Object.entries(array)) {
+      if (id in result) {
+        result[id] += count;
+      } else if (!(id in result)) {
+        result[id] = count;
+      }
+    }
+  }
+  return result;
+}
+
+module.exports = mergedResults;
