f979fe199a1760b08089009aeac53ac13752dead
DenisEps <denis.epshteyn@gmail.com>
2020-08-27 14:10:24 +0300

    adds repost counter

--- count-activity/countActivity.js
+++ count-activity/countActivity.js
@@ -1,16 +1,11 @@
 const fetch = require('node-fetch');
 
-const token = '0d50473f2681355889a936e3c5a29d5b38c2e98f4ae8afd897a88fa619e0293537ee4940eb2187940a336';
-const groupId = '-117680198';
-// const groupId = '-198233594'
-// const postIds = ['1']
-const postIds = ['20844', '20826', '20818'];
-
 const userLikes = {};
 const userComments = {};
+const userPosts = {};
 
 // LIKES >>>
-async function countLikesForPost(postId) {
+async function countLikesForPost(postId, token, groupId) {
   const response = await fetch(`https://api.vk.com/method/likes.getList?&type=post&access_token=${token}&owner_id=${groupId}&item_id=${postId}&offset=0&v=5.122&count=1000`);
   const final = await response.json();
   const likesArray = final.response.items;
@@ -24,18 +19,19 @@ async function countLikesForPost(postId) {
   }
 }
 
-async function countLikesAllPosts(postsArray) {
+async function countLikesAllPosts(postsArray, token, groupId) {
   for (const postId of postsArray) {
-    await countLikesForPost(postId)
+    await countLikesForPost(postId, token, groupId)
   }
+  return userLikes;
 }
 
 // COMMENTS >>>
 // TODO comments for comments?
-async function countCommentsForPost(postId) {
+async function countCommentsForPost(postId, token, groupId) {
   const response = await fetch(`https://api.vk.com/method/wall.getComments?&owner_id=${groupId}&post_id=${postId}&need_likes=1&access_token=${token}&v=5.122&count=100`)
   const final = await response.json();
-  const commentsArray = final.response.items;
+  const commentsArray = final.response.items
 
   for (const commentObj of commentsArray) {
     if (commentObj.from_id in userComments) {
@@ -46,14 +42,34 @@ async function countCommentsForPost(postId) {
   }
 }
 
-async function countCommentsAllPosts(postsArray) {
+async function countCommentsAllPosts(postsArray, token, groupId) {
   for (const postId of postsArray) {
-    await countCommentsForPost(postId)
+    await countCommentsForPost(postId, token, groupId)
   }
+  return userComments;
 }
 
 // REPOSTS >>>
-// ???????????
+async function countRepostsForPost(postId, token, groupId) {
+  const response = await fetch(`https://api.vk.com/method/wall.getReposts?&access_token=${token}&owner_id=${groupId}&post_id=${postId}&offset=0&v=5.122&count=1000`)
+  const final = await response.json();
+  const commentsArray = final.response.items
 
-countLikesAllPosts(postIds).then(_ => console.log(userLikes));
-countCommentsAllPosts(postIds).then(_ => console.log(userComments));
+  for (const responseObj of commentsArray) {
+    if (responseObj.from_id in userPosts) {
+      userPosts[responseObj.from_id] += 1;
+    } else if (!(responseObj.from_id in userPosts)) {
+      userPosts[responseObj.from_id] = 1;
+    }
+  }
+}
+
+async function countRepostsAllPosts(postsArray, token, groupId) {
+  for (const postId of postsArray) {
+    await countRepostsForPost(postId, token, groupId)
+  }
+  return userPosts;
+}
+
+
+module.exports = { countLikesAllPosts, countCommentsAllPosts, countRepostsAllPosts }
