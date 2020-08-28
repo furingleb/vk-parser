73114c3c0dac7baf7c5a8d79e5e4ceacdfad00e7
Ivan <ivan070797@yandex.ru>
2020-08-28 15:19:45 +0300

    env

--- middleware/index.js
+++ middleware/index.js
@@ -12,7 +12,7 @@ module.exports = (app) => {
     passport.use(new VKontakteStrategy({
         clientID: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENTSECRET,
-        callbackURL: `http://statyou.herokuapp.com/auth/vkontakte/callback`
+        callbackURL: `${process.env}auth/vkontakte/callback`
     },
 
         async function (accessToken, refreshToken, params, profile, done) {
