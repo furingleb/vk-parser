0043fa22fe2b1e96c53f3d477ec234fb8c06e35d
DenisEps <denis.epshteyn@gmail.com>
2020-08-28 16:44:37 +0300

    theme table

--- public/currentTheme.js
+++ public/currentTheme.js
@@ -1,7 +1,11 @@
-if(localStorage.theme === 'light'){
-    console.log('light');
-    body.classList.add('lightTheme')
+const table = document.getElementById('table');
+
+if (localStorage.theme === 'light') {
+  console.log('light');
+  body.classList.add('lightTheme')
+  table.classList.add('table-dark')
 }
-if(localStorage.theme === 'dark') {
-    body.classList.remove('lightTheme')
+if (localStorage.theme === 'dark') {
+  body.classList.remove('lightTheme')
+  table.classList.remove('table-dark')
 }
--- public/themes.js
+++ public/themes.js
@@ -21,6 +21,7 @@ lightThemeBtn.addEventListener('click', () => {
 if(localStorage.theme === 'light'){
     console.log('light');
     body.classList.add('lightTheme')
+    
 }
 if(localStorage.theme === 'dark') {
     body.classList.remove('lightTheme')
